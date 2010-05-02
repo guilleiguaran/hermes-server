80449b2e1ef4964b727ece95e64ada1153eb6705
Guillermo <guilleiguaran@gmail.com>
2010-05-01 19:08:44 -0500

    Adding algorithm

---
+++ src/main/scala/com/hermes/hermesd/algorithm/Algorithm.scala
@@ -0,0 +1,26 @@
+package com.hermes.hermesd.algorithm
+
+class Node(var Coordinates: List[Int]){
+
+    var costsAdjacents = new ArrayList[Double]();
+    var nodesAdjacents = new ArrayList[Node]()
+    var gScore = 0.0
+    var hScore = 0.0
+    var fScore = 0.0
+    var cameFrom = List(-1,-1)
+
+    def aString(complet: Boolean): String = {
+        if(complet == true){
+            "(gScore, hScore, fScore) = (" + gScore + "," + hScore + "," + fScore + ") --- " +
+            "(x,y) = (" + Coordinates(0) + "," + Coordenates(1) + ")"
+        }else{
+            "(" + Coordinates(0) + "," + Coordinates(1) + ")"    
+        }
+    }
+
+    def addAdjacent(node: Node, cost: Double){
+        costsAdjacents.add(cost)
+        nodesAdjacents.add(node)
+    }
+
+}

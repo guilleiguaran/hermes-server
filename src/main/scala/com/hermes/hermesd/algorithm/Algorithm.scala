151468d0f2412b9027c28d7fdf97e1cd7bcf82ce
root <root@lacides-laptop.(none)>
2010-06-10 20:24:54 -0500

    Correccion del algoritmo

--- src/main/scala/com/hermes/hermesd/algorithm/Algorithm.scala
+++ src/main/scala/com/hermes/hermesd/algorithm/Algorithm.scala
@@ -45,7 +45,17 @@ class Node(var dbId: String) extends Comparable[Node]{
     }
 }
 
-
+/*
+var t = DataConnection.cassandra.ColumnFamily("Standard1").elements
+	var Salida = ""
+	//while(t.hasNext){
+	//	Salida = Salida + ";" + (new Node(t.next)).aString(false)
+	//}
+	while(t.hasNext){
+		Salida = Salida + ";" +  (new Node(t.next._1)).aString(false)
+	}
+	Salida
+*/
 class AStar(var minCost: Double){
     def getAllNodes(): String = {
 	var t = DataConnection.cassandra.ColumnFamily("Standard1").toArray
@@ -62,10 +72,27 @@ class AStar(var minCost: Double){
 
     //def heuristicStimateOfDistance(a: List[Int], b: List[Int]) = minCost * (Math.abs(a(0) - b(0)) + Math.abs(a(1) - b(1)))
     def heuristicStimateOfDistance(aI : String, bI: String): Double = {
-		var a = DataConnection.cassandra.ColumnFamily("Standard1")(aI)
+
+/*		var a = DataConnection.cassandra.ColumnFamily("Standard1")(aI)
 		var b = DataConnection.cassandra.ColumnFamily("Standard1")(bI)	
 		var r = minCost * (Math.abs(a("Lat").toDouble - b("Lat").toDouble) + Math.abs(a("Lon").toDouble - b("Lon").toDouble))
 		r
+*/
+		val f = DataConnection.cassandra.ColumnFamily("Standard1")(aI)
+		val t = DataConnection.cassandra.ColumnFamily("Standard1")(bI)
+		
+		var dlong = f("Lon").toDouble - t("Lon").toDouble
+
+		var degtorad = 0.01745329
+		var radtodeg = 57.29577951
+
+		var dvalue = (Math.sin(f("Lat").toDouble * degtorad))*(Math.sin(t("Lat").toDouble * degtorad)) + 
+		(Math.cos(f("Lat").toDouble * degtorad) * Math.cos(t("Lat").toDouble * degtorad) * Math.cos(dlong * degtorad))
+
+		var dd = Math.acos(dvalue) * radtodeg
+
+		var km = dd*111.302 
+		km/30.0
     }
 
     def buildPath(current: Node, closedset: HashMap[String, Node]): String = {
@@ -87,14 +114,26 @@ class AStar(var minCost: Double){
         	var startId = NearestNeighbor.find(start,0.0001)
 		var endId = NearestNeighbor.find(goal,0.0001)
 
+//		println("Meta :" + endId)
+//		println("Inicio:" + startId)
+
         openset.add(new Node(startId))
         
         while(openset.isEmpty() == false){
 
             var x:Node = openset.poll()
+//	    println("En el nodo :" + x.dbId)
     	    if(x.dbId == endId){
                 //return buildPath(x,closedset)
-				closedset(x.dbId) = x
+		if(closedset.contains(x.dbId)){
+		 	if(closedset(x.dbId).gScore > x.gScore){
+				closedset(x.dbId) = x
+			}
+		}else{
+			 closedset(x.dbId) = x
+		}
+//		println("Llego, a traves de " + x.cameFrom)		
+//	    	Console.readLine()
             }
 	    else {
 
@@ -111,30 +150,36 @@ class AStar(var minCost: Double){
                 de esa clase
                 */
                 var i = x.getNeighbors();
+//		println("Expandiendo")
                 //var j = x.costsAdjacents.iterator();
                 while(i.hasNext) {
                     var currentNode:Node = new Node(i.next)
+//		    println("Hijo " + currentNode.dbId)
                     var dist:Double = DataConnection.cassandra.SuperColumnFamily("Super1")(x.dbId.toString)(currentNode.dbId.toString)(hora.toString).toDouble
-                    if(closedset.contains(currentNode.dbId) == false){
-	
+
+                    if(closedset.contains(currentNode.dbId) == false){			
                     	var tgScore = x.gScore + dist
                         var tIsBetter = false
                         if(openset.contains(currentNode) == false){
                             openset.add(currentNode)
                             tIsBetter = true
                         }
-						else {
+			else {
+			    println("Ya habia llegado")
                             if(tgScore < currentNode.gScore){
+				println("Este es mejor")
                                 tIsBetter = true
                             }
-							else {
+			    else {
+				println("El anterior es mejor")
                                 tIsBetter = false
                             }
+	    		    Console.readLine()
                         }
                         if(tIsBetter){
                             currentNode.cameFrom_=(x.dbId)
                             currentNode.gScore_=(tgScore)
-                            currentNode.hScore_=(heuristicStimateOfDistance(x.dbId, currentNode.dbId))
+                            currentNode.hScore_=(heuristicStimateOfDistance(endId, currentNode.dbId))
                             currentNode.fScore_=(currentNode.gScore + currentNode.hScore)
                         }
                     }

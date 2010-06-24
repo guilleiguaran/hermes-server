f5a928578fa7386f23f2d2b9005f0d7751ee7cac
Luis Alfredo Porras Páez <lporras@lporras-laptop-lucid.(none)>
2010-06-24 17:12:00 -0500

    Fixing the algorithm

--- src/main/scala/com/hermes/hermesd/algorithm/Algorithm.scala
+++ src/main/scala/com/hermes/hermesd/algorithm/Algorithm.scala
@@ -1,5 +1,4 @@
 package com.hermes.hermesd.algorithm
-
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.mutable.HashMap 
 import java.util.ArrayList
@@ -24,13 +23,14 @@ object DataConnection{
 }
 
 
-class Node(var dbId: String) extends Comparable[Node]{
+class Node(var dbId: String, var cameFrom: Node) extends Comparable[Node]{
     var gScore = 0.0
     var hScore = 0.0
     var fScore = 0.0
-    var cameFrom = "-1"
 
     def compareTo(other: Node): Int = { this.fScore.compare(other.fScore) }
+	
+    def equals(other: Node):Boolean = {this.dbId.equals(other.dbId)}
     
     def getNeighbors(): java.util.Iterator[String] = { 
 	var vecinos = new ArrayList[String]()
@@ -48,16 +48,17 @@ class Node(var dbId: String) extends Comparable[Node]{
     
     def aString(complete: Boolean): String = {
         if(complete == true){
-            "(gScore, hScore, fScore) = (" + gScore + "," + hScore + "," + fScore + ") --- " +
-            aString(false)
+            "(gScore, hScore, fScore) = (" + gScore + "," + hScore + "," + fScore + ") ---"+ aString(false)
         }
 		else {
            DataConnection.cassandra.ColumnFamily("Coordenadas")(dbId)("Lat") + "_" +  DataConnection.cassandra.ColumnFamily("Coordenadas")(dbId)("Lon")   
         }
     }
+
 }
 
 class AStar(var minCost: Double){
+
     def getAllNodes(): String = {
 
 	var size = DataConnection.cassandra.ColumnFamily("Intersecciones")("DB")("0").toInt
@@ -91,106 +92,104 @@ class AStar(var minCost: Double){
 		horas*3600
     }
 
-    def buildPath(current: Node, closedset: HashMap[String, Node]): String = {
+    def buildPath(current: Node): String = {
 	println(current.dbId)	
-	if (current.cameFrom != "-1"){
+	if (current.cameFrom != null){
 			
-			return buildPath(closedset(current.cameFrom), closedset)  + ";" +current.aString(false)
+			return buildPath(current.cameFrom)  + ";" +current.aString(false)
         }
 	else {
             return current.aString(false)
         }   
     }
     
+    def vecinos(nodo: Node, hora: Int, fin: String):ArrayList[Node]={
+        var i = nodo.getNeighbors();
+	println("Expandiendo")
+	var lista = new ArrayList[Node]()
+        
+        while(i.hasNext) {
+	    
+	    var currentNode = new Node(i.next, nodo)
+	    println("nodo: "+currentNode.dbId)
+	    var dist:Double = DataConnection.cassandra.SuperColumnFamily("Trafico")(nodo.dbId.toString)(currentNode.dbId.toString)(hora.toString).toDouble
+	    currentNode.gScore = nodo.gScore + dist
+	    currentNode.hScore = heuristicStimateOfDistance(currentNode.dbId, fin)
+	    currentNode.fScore = currentNode.gScore + currentNode.hScore
+	    lista.add(currentNode)
+	}
+	return lista	
+    }
+
+
+    def gestionarVecinos(hijos: ArrayList[Node], abierta: PriorityQueue[Node], abiertahashmap: HashMap[String, Node], cerrada: HashMap[String, Node]){
+
+	for(i<-0 to hijos.size - 1){
+          var hijoActual = hijos.get(i).asInstanceOf[Node]	
+          
+	   if(cerrada.contains(hijoActual.dbId) == false){
+
+	    if(abiertahashmap.contains(hijoActual.dbId) == false){
+		
+		abierta.add(hijoActual)
+		abiertahashmap(hijoActual.dbId) = hijoActual
+	     }
+	     else{
+		var nodoEnAbierta = abiertahashmap(hijoActual.dbId)	
+		
+		if(hijoActual.gScore < nodoEnAbierta.gScore){
+			abierta.remove(nodoEnAbierta)
+			nodoEnAbierta.gScore = hijoActual.gScore
+			nodoEnAbierta.fScore = hijoActual.fScore
+			abierta.add(nodoEnAbierta)
+		} 
+	     }
+	  }	
+	}
+      
+    }
+
     def calculatePath(start: Map[String, String], goal: Map[String, String], hora: Int): String = {
-        var nodes = new HashMap[String, Node]()
 
 	var closedset = new HashMap[String, Node]()
- 
-	var openset = new PriorityQueue[Node]()
+ 	var openset = new PriorityQueue[Node]()
+	var opensethashmap = new HashMap[String, Node]()
         var startId = NearestNeighbor.find(start,0.0001)
 	var endId = NearestNeighbor.find(goal,0.0001)
 
-//	println("Meta :" + endId)
-//	println("Inicio:" + startId)
+	println("Meta :" + endId)
+	println("Inicio:" + startId)
+	var NodoInicio = new Node(startId, null)
+	NodoInicio.hScore = heuristicStimateOfDistance(startId, endId)
+	NodoInicio.fScore = NodoInicio.gScore + NodoInicio.hScore
+	//var NodoFin = new Node(endId, null)
 	
-	nodes(startId) = new Node(startId)
-        openset.add(nodes(startId))
-       
-        while(openset.isEmpty() == false){
+	// Añade el nodo inicial a la lista cerrada.
+	closedset(startId) = NodoInicio
+	
+	//openset.addAll(vecinos(NodoInicio, hora, NodoFin))
+         var hijosDeInicio = vecinos(NodoInicio, hora, endId)
+	 gestionarVecinos(hijosDeInicio, openset, opensethashmap, closedset)
 
-            var x:Node = openset.poll()
-//	    println("En el nodo :" + x.dbId)
-    	    if(x.dbId == endId){
-                //return buildPath(x,closedset)
-		if(closedset.contains(x.dbId)){
-		 	if(closedset(x.dbId).gScore > x.gScore){
-				closedset(x.dbId) = x
-			}
-		}else{
-			 closedset(x.dbId) = x
-		}
-//		println("Llego, a traves de " + x.cameFrom)		
-//	    	Console.readLine()
-            }
-	    else {
-
-                closedset(x.dbId) = x
-
-                var i = x.getNeighbors();
-//		println("Expandiendo")
-                //var j = x.costsAdjacents.iterator();
-                while(i.hasNext) {
-	            var currentId = i.next
-		    var currentNode:Node = {
-	  		    if(nodes.contains(currentId)){
-				nodes(currentId)
-			    }else{
-				nodes(currentId) = new Node(currentId)
-				nodes(currentId)
-			    }
-		    }
-
-//                    var currentNode:Node = new Node(i.next)
-//		    println("Hijo " + currentNode.dbId)
-                    var dist:Double = DataConnection.cassandra.SuperColumnFamily("Trafico")(x.dbId.toString)(currentNode.dbId.toString)(hora.toString).toDouble
-
-                    if(closedset.contains(currentNode.dbId) == false){			
-                    	var tgScore = x.gScore + dist
-                        var tIsBetter = false
-		        
-                        if(openset.contains(currentNode) == false){
-                            openset.add(currentNode)
-                            tIsBetter = true
-//			    println("Primera vez")
-                        }
-			else{
-//			    println("Ya habia llegado")
-                            if(tgScore < currentNode.gScore){
-//				println("Este es mejor")
-                                tIsBetter = true
-                            }
-			    else {
-//				println("El anterior es mejor")
-                                tIsBetter = false
-                            }
-//			    println(tgScore)
-//			    println(currentNode.gScore)
-                        }
-//	    		    Console.readLine()
-
-                        if(tIsBetter){
-                            currentNode.cameFrom_=(x.dbId)
-                            currentNode.gScore_=(tgScore)
-                            currentNode.hScore_=(heuristicStimateOfDistance(endId, currentNode.dbId))
-                            currentNode.fScore_=(currentNode.gScore + currentNode.hScore)
-                        }
-                    }
-
-                }
-            }
-        }
-	return buildPath(closedset(endId),closedset)
+	while(opensethashmap.contains(endId) || openset.isEmpty == false){
+	 	  
+	  var x:Node = openset.poll()
+	  opensethashmap.removeKey(x.dbId)
+	  println("X ES " + x.dbId)
+	  closedset(x.dbId) = x
+	  var hijosdeX = vecinos(x, hora, endId)
+	  gestionarVecinos(hijosdeX, openset, opensethashmap, closedset)
+	  println("tamaño openset: "+openset.size)
+	  println("tamaño openset: "+opensethashmap.size)
+	  println("tamaño closedset: "+closedset.size)	  	
+	}
+	
+	  return buildPath(closedset(endId))
+	
     }
+
 }
+
+
+
 
